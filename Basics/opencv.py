a6e60f6f9ea9044cfcb71e5b8120f68300276b2a
Saugat <saugatkafley@gmail.com>
2022-01-01 21:43:53 +0545

    cv2.resize

--- Basics/opencv.py
+++ Basics/opencv.py
@@ -1,7 +1,7 @@
 import cv2
 
 frameWidth  = 640
-frameHeight = 360
+frameHeight = 480
 
 cap  = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
@@ -9,6 +9,7 @@ cap.set(4, frameHeight)
 
 while(True):
     sucess , img = cap.read()
+    img = cv2.resize(img, (frameWidth, frameHeight))
     img  =cv2.imshow("Video", img)
     if cv2.waitKey(2)  &  0xFF == ord('q'):
         break
