3a121b401177b5f4b263eb6145ab135ef33b20e2
Saugat <saugatkafley@gmail.com>
2022-01-01 22:01:27 +0545

    cv BGR2RGB function to turn strange to frost giant

--- Basics/cv1.py
+++ Basics/cv1.py
@@ -1,8 +1,12 @@
 import cv2
 
+fieldWidth = 720
+fieldHeight = 480
 path_img  = "Resources/doctor strange.jpg"
 path_mp4 = "Resources/test_video.mp4"
 
 img  = cv2.imread(path_img)
+img_greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 cv2.imshow("Image", img)
+cv2.imshow("Image", img_greyscale)
 cv2.waitKey(0)
