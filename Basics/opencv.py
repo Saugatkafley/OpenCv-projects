4684491849be1aeb609e46b9505a002724ef7492
Saugat <saugatkafley@gmail.com>
2022-01-01 21:40:14 +0545

    Read webcam and video from Resources

--- Basics/opencv.py
+++ Basics/opencv.py
@@ -1,7 +1,14 @@
 import cv2
 
-# reading image from resources folder
-img  = cv2.imread("Resources/doctor strange.jpg")
+frameWidth  = 640
+frameHeight = 360
 
-cv2.imshow("Doctor strange", img)
-cv2.waitKey(0)
+cap  = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+while(True):
+    sucess , img = cap.read()
+    img  =cv2.imshow("Video", img)
+    if cv2.waitKey(2)  &  0xFF == ord('q'):
+        break
