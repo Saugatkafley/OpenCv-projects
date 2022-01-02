2cd78fe8163ee7d9aed583a77b8ba2cc6655b988
Saugat Kafley <saugatkafley@gmail.com>
2022-01-02 17:32:58 +0545

    Image cropped and resized

--- Basics/cv_crop_resize.py
+++ Basics/cv_crop_resize.py
@@ -2,7 +2,14 @@ import cv2
 import numpy as np
 
 img_path = "Resources/doctor strange.jpg"
-
+width , height  = 500,500
 img  = cv2.imread(img_path)
+img_resized = cv2.resize(img ,(width,height) )
+
+image_Cropped = img[0:900, 400:1000]            # img[height, width] , y,x
+print(img.shape)                                # (900,1600, 3)
+
 cv2.imshow("Original", img)
+cv2.imshow("Image Resized", img_resized)
+cv2.imshow("Image Cropped", image_Cropped)
 cv2.waitKey(0)
