bd2c67d9ad56a0222bef5da8927bba8643e4efe5
Saugat Kafley <saugatkafley@gmail.com>
2022-01-05 17:57:16 +0545

    Comment to warp.py

--- Real Time Color Detection/color_detect.py
+++ Real Time Color Detection/color_detect.py
@@ -5,6 +5,10 @@ cap  = cv2.VideoCapture(0)
 
 while True:
     sucess , img  = cap.read()
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    
+    
     cv2.imshow('WebCam', img)
+    cv2.imshow('WebCam HSV', imgHSV)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
