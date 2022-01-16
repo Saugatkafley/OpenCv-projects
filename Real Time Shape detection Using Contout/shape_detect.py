29b4bd898ea314d531d21ba89b9c937911877c1a
Saugat <saugatkafley@gmail.com>
2022-01-16 18:47:20 +0545

    Parameters trackbars

--- Real Time Shape detection Using Contout/shape_detect.py
+++ Real Time Shape detection Using Contout/shape_detect.py
@@ -9,18 +9,26 @@ frameHeight = 480
 
 
 cap  = cv2.VideoCapture(0)
-# cap.set(3, frameWidth)
-# cap.set(4, frameHeight)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+def empty(a):
+    pass
+#Parameters for the contour detection
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters" , frameWidth , 80)
+cv2.createTrackbar("Threshold1", "Parameters" , 150 , 255 , empty)
+cv2.createTrackbar("Threshold2", "Parameters" , 255 , 255 , empty)
 
 while True:
     sucess ,img = cap.read()
     img_blur  = cv2.GaussianBlur(img,(7,7),1)
     img_gray  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
-    output = stackImages(  ( [img] ), scale=0.5) 
+    output = stackImages(  ( [img , img_blur , img_gray] ), scale=1) 
     # cv2.imshow("WebCam" , img)
     # cv2.imshow("WebCam Blur" , img_blur)
     # cv2.imshow("WebCam Gray" , img_gray)
+
     cv2.imshow("WebCam Stacked" , output)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
