869c8f06732b3cc31f513f0977d5503c117586d7
Saugat <saugatkafley@gmail.com>
2022-01-16 19:53:38 +0545

    Get contours

--- Real Time Shape detection Using Contout/shape_detect.py
+++ Real Time Shape detection Using Contout/shape_detect.py
@@ -4,13 +4,13 @@ import sys
 sys.path.append('Others')
 from Utils import *     # Thanks to Co-Pilot
 
-frameWidth = 640
+frameWidth = 480
 frameHeight = 480
 
 
-cap  = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
+cap  = cv2.VideoCapture(1)
+# cap.set(3, frameWidth)
+# cap.set(4, frameHeight)
 
 def empty(a):
     pass
@@ -18,19 +18,27 @@ def empty(a):
 #Parameters for the contour detection
 cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters" , frameWidth , 80)
-cv2.createTrackbar("Threshold1", "Parameters" , 150 , 255 , empty)
-cv2.createTrackbar("Threshold2", "Parameters" , 255 , 255 , empty)
+cv2.createTrackbar("Threshold1", "Parameters" , 131 , 255 , empty) # 131 is the base value
+cv2.createTrackbar("Threshold2", "Parameters" , 202 , 255 , empty) # 202 is the base value
 
+def getContours(img , imgContour):
+
+    contours , hierarchy = cv2.findContours(img , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(imgContour , contours , -1 , (255,0,0) , 3)
 while True:
     sucess ,img = cap.read()
     img_blur  = cv2.GaussianBlur(img,(7,7),1)
     img_gray  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
     threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
     threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
     
-    
     img_canny  = cv2.Canny(img_gray , threshold1 , threshold2)
-    output = stackImages(  ( [img , img_canny , img_gray] ,[img , img_canny , img_blur]), scale=0.1) 
+    kernel    = np.ones((5,5))
+    img_dilate = cv2.dilate(img_canny, kernel, iterations=1)
+    
+    getContours(img_dilate, img)
+    output = stackImages(  ( [img , img_canny , img_dilate] ,[img_dilate , img_canny , img_blur]), scale=0.5)
     # cv2.imshow("WebCam" , img)
     # cv2.imshow("WebCam Blur" , img_blur)
     # cv2.imshow("WebCam Gray" , img_gray)
