d6ea5e29494a8b292f68b34b0c26fcf3b38b273f
Saugat <saugatkafley@gmail.com>
2022-01-18 20:49:14 +0545

    Added opencv-contrib dependicies

--- Tracking Objects/tracking.py
+++ Tracking Objects/tracking.py
@@ -2,13 +2,18 @@ import cv2
 import numpy as np
 
 #Frame Width and Height
-framewidth  , frameheight = 300,300
+framewidth  , frameheight = 400,400
 cap  = cv2.VideoCapture(0)
 cap.set(3, framewidth)
 cap.set(4, frameheight)
 
+cv2.TrackerMOSSE_create()
+
 while True:
+    timer  = cv2.getTickCount()
     sucess , img  = cap.read()
+    fps   = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    cv2.putText(img, str(int (fps)) , (10,50) , cv2.FONT_HERSHEY_COMPLEX , 1 , (0,255,0) , 2)
     cv2.imshow("WebCam" , img)
-    if cv2.waitKey(1) & 0xFF  == ord('q'):
+    if cv2.waitKey(2) & 0xFF  == ord('q'):
         break
