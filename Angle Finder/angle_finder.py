a29688bc94c8e53f78c22db51cc4250233fbb67a
Saugat <saugatkafley@gmail.com>
2022-01-21 16:16:41 +0545

    Added angle measurement

--- Angle Finder/angle_finder.py
+++ Angle Finder/angle_finder.py
@@ -1,30 +1,43 @@
+from cmath import atan
 import cv2
 import numpy as np
-
-path_img = "Resources/shape1.png"
+import math
+path_img = "Resources/shape2.png"
 img  = cv2.imread(path_img)
 
 pointsList = []
 def mousePoints(event, x,y, flags , params):
     if event == cv2.EVENT_LBUTTONDOWN: 
         pointsList.append([x,y])
-        cv2.circle(img, (x,y), 5, (0,0,255), -1)
+        cv2.circle(img, (x,y), 7, (0,255,0), -1)
         print(pointsList)
 
-def get_angle():
-    print("angle")
+def get_angle(m1, m2):
+    tanR  = math.atan((m1-m2)/(1+m1*m2) ) 
+    tanD  = round(math.degrees(tanR))
+    print("angle :", tanD)
+    return tanD
+
+def get_slope(x1,y1,x2,y2):
+    m = (y2-y1)/(x2-x1)
+    return m
 
 while True:
     
     if (len(pointsList) % 3 ) == 0 and len(pointsList) != 0:
-        get_angle()
+        m1 = get_slope(pointsList[0][0],pointsList[0][1],pointsList[1][0],pointsList[1][1])
+        m2 = get_slope(pointsList[1][0],pointsList[1][1],pointsList[2][0],pointsList[2][1])
+        tan0 = get_angle(m1,m2)
+        cv2.putText(img, "angle :" + str(tan0), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
     if len(pointsList) <4:
         cv2.imshow("Shape" , img)
         cv2.setMouseCallback("Shape" , mousePoints) # set mouse callback on window Image, call mousePoints 
         cv2.waitKey(1)
         # reset pointlist and image
-        if cv2.waitKey(1) &  0xFF == ord('q'):
+        if cv2.waitKey(1) &  0xFF == ord('r'):
             pointsList = []
             img  = cv2.imread(path_img)
     else:
+        break
+    if cv2.waitKey(1) &  0xFF == ord('b'):
         break
