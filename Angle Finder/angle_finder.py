cc40d2e45a71a2aa9b3f0707c9ad2f54bbf72af5
Saugat <saugatkafley@gmail.com>
2022-01-21 15:20:54 +0545

    Image output ~angle finder

--- Angle Finder/angle_finder.py
+++ Angle Finder/angle_finder.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
+path_img = "Resources/shape1.png"
+img  = cv2.imread(path_img)
 
-img  = cv2.imread("Resources/shape1.png")
-img.set(3, 500)
-img.set(4, 500)
 
 cv2.imshow("Image" , img)
 cv2.waitKey(0)
